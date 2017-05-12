da32f11d89dae9e8c8a6ab1a552d2fb01d86b0bb
aburns <anthony@clearme.com>
2017-05-12 17:43:29 -0400

    complete 6/10

--- timeusage/src/main/scala/timeusage/TimeUsage.scala
+++ timeusage/src/main/scala/timeusage/TimeUsage.scala
@@ -2,6 +2,7 @@ package timeusage
 
 import java.nio.file.Paths
 
+import org.apache.spark.rdd.RDD
 import org.apache.spark.sql._
 import org.apache.spark.sql.types._
 
@@ -34,11 +35,30 @@ object TimeUsage {
     finalDf.show()
   }
 
+
   /** @return The read DataFrame along with its column names. */
   def read(resource: String): (List[String], DataFrame) = {
-    val rdd = spark.sparkContext.textFile(fsPath(resource))
+    val r = rdd(resource)
 
-    val headerColumns = rdd.first().split(",").to[List]
+    read(headerColumns(r), r)
+  }
+
+  def rdd(resource: String): RDD[String] = {
+    spark.sparkContext.textFile(fsPath(resource))
+  }
+
+  def read(resource: String, size: Double): (List[String], DataFrame) = {
+    val r = rdd(resource)
+
+    read(headerColumns(r), r.sample(false, size))
+  }
+
+
+  private def headerColumns(rdd: RDD[String]): List[String] = {
+    rdd.first().split(",").to[List]
+  }
+
+  private def read(headerColumns: List[String], rdd: RDD[String]): (List[String], DataFrame) = {
     // Compute the schema based on the first line of the CSV file
     val schema = dfSchema(headerColumns)
 
@@ -64,64 +84,74 @@ object TimeUsage {
     */
   def dfSchema(columnNames: List[String]): StructType = {
     StructType(
-      columnNames.zipWithIndex.map(f => f._2 match {
-        case 0 => StructField (f._1, StringType, true)
-        case _ => StructField (f._1, DoubleType, true)
-      }))
+      StructField(columnNames.head, StringType, false) ::
+        columnNames.tail.map(StructField(_, DoubleType, false))
+    )
   }
-
 
 
   /** @return An RDD Row compatible with the schema produced by `dfSchema`
     * @param line Raw fields
     */
-  def row(line: List[String]): Row ={
+  def row(line: List[String]): Row = {
     Row.fromSeq(
-      line)
+      line.head :: line.tail.map(_.toDouble))
   }
 
   /** @return The initial data frame columns partitioned in three groups: primary needs (sleeping, eating, etc.),
     *         work and other (leisure activities)
-    *
     * @see https://www.kaggle.com/bls/american-time-use-survey
     *
-    * The dataset contains the daily time (in minutes) people spent in various activities. For instance, the column
-    * “t010101” contains the time spent sleeping, the column “t110101” contains the time spent eating and drinking, etc.
+    *      The dataset contains the daily time (in minutes) people spent in various activities. For instance, the column
+    *      “t010101” contains the time spent sleeping, the column “t110101” contains the time spent eating and drinking, etc.
     *
-    * This method groups related columns together:
+    *      This method groups related columns together:
     * 1. “primary needs” activities (sleeping, eating, etc.). These are the columns starting with “t01”, “t03”, “t11”,
-    *    “t1801” and “t1803”.
+    *      “t1801” and “t1803”.
     * 2. working activities. These are the columns starting with “t05” and “t1805”.
     * 3. other activities (leisure). These are the columns starting with “t02”, “t04”, “t06”, “t07”, “t08”, “t09”,
-    *    “t10”, “t12”, “t13”, “t14”, “t15”, “t16” and “t18” (those which are not part of the previous groups only).
+    *      “t10”, “t12”, “t13”, “t14”, “t15”, “t16” and “t18” (those which are not part of the previous groups only).
     */
   def classifiedColumns(columnNames: List[String]): (List[Column], List[Column], List[Column]) = {
-    classifiedColumns(columnNames, (Nil,Nil,Nil))
+    classifiedColumns(columnNames, (Nil, Nil, Nil))
   }
 
-  def classifiedColumns(columnNames: List[String], result:(List[Column], List[Column], List[Column])): (List[Column], List[Column], List[Column]) = columnNames match {
+  def classifiedColumns(columnNames: List[String], result: (List[Column], List[Column], List[Column])): (List[Column], List[Column], List[Column]) = columnNames match {
     case Nil => result
-    case h::t => h match {
-      case "t01" | "t03" | "t11" | "t1801" | "t1803" => classifiedColumns(t, (col(h)::result._1, result._2, result._3))
-      case "t05" | "t1805" => classifiedColumns(t, (result._1, col(h)::result._2, result._3))
-      case "t02"|"t04"|"t06"|"t07"|"t08"|"t09"|"t10"|"t12"|"t13"|"t14"|"t15"|"t16"|"t18"  => classifiedColumns(t, (result._1, result._2, col(h)::result._3))
+    case h :: t => h match {
+      case s if startsWith(h, List("t01", "t03", "t11", "t1801", "t1803")) => classifiedColumns(t, (col(h) :: result._1, result._2, result._3))
+      case s if startsWith(h, List("t05", "t1805")) => classifiedColumns(t, (result._1, col(h) :: result._2, result._3))
+      case s if startsWith(h, List("t02", "t04", "t06", "t07", "t08", "t09", "t10", "t12", "t13", "t14", "t15", "t16", "t18")) => classifiedColumns(t, (result._1, result._2, col(h) :: result._3))
       case _ => classifiedColumns(t, result)
     }
   }
 
+  def startsWith(c: String, l: List[String]): Boolean = l match {
+    case Nil => false
+    case h :: t => {
+      if (c.startsWith(h)) true
+      else startsWith(c, t)
+    }
+  }
+
+  //  s.startsWith("t03") | s.startsWith("t11") | s.startsWith("t1801") | s.startsWith("t1803") => classifiedColumns(t, (col(h) :: resuls._1, resuls._2, resuls._3))
+  //  case s if s.startsWith("t05") | s.startsWith("t1805") => classifiedColumns(t, (resuls._1, col(h) :: resuls._2, resuls._3))
+  //  case s if s.startsWith("t02") | s.startsWith("t04") | s.startsWith("t06") | s.startsWith("t07")| s.startsWith("t08") | s.startsWith("t09") | s.startsWith("t10") | s.startsWith("t12") | s.startsWith("t13") | t.startsWith("t14") | t.startsWith("t15") | t.startsWith("t16") | t.startsWith("t18") => classifiedColumns(t, (result._1, result._2, col(h) :: result._3))
+  //  case _ => classifiedColumns(t, result)
+  //}
+
   /** @return a projection of the initial DataFrame such that all columns containing hours spent on primary needs
     *         are summed together in a single column (and same for work and leisure). The “teage” column is also
     *         projected to three values: "young", "active", "elder".
-    *
     * @param primaryNeedsColumns List of columns containing time spent on “primary needs”
-    * @param workColumns List of columns containing time spent working
-    * @param otherColumns List of columns containing time spent doing other activities
-    * @param df DataFrame whose schema matches the given column lists
+    * @param workColumns         List of columns containing time spent working
+    * @param otherColumns        List of columns containing time spent doing other activities
+    * @param df                  DataFrame whose schema matches the given column lists
     *
-    * This methods builds an intermediate DataFrame that sums up all the columns of each group of activity into
-    * a single column.
+    *                            This methods builds an intermediate DataFrame that sums up all the columns of each group of activity into
+    *                            a single column.
     *
-    * The resulting DataFrame should have the following columns:
+    *                            The resulting DataFrame should have the following columns:
     * - working: value computed from the “telfs” column of the given DataFrame:
     *   - "working" if 1 <= telfs < 3
     *   - "not working" otherwise
@@ -135,25 +165,44 @@ object TimeUsage {
     * - work: sum of all the `workColumns`, in hours
     * - other: sum of all the `otherColumns`, in hours
     *
-    * Finally, the resulting DataFrame should exclude people that are not employable (ie telfs = 5).
+    *                            Finally, the resulting DataFrame should exclude people that are not employable (ie telfs = 5).
     *
-    * Note that the initial DataFrame contains time in ''minutes''. You have to convert it into ''hours''.
+    *                            Note that the initial DataFrame contains time in ''minutes''. You have to convert it into ''hours''.
     */
   def timeUsageSummary(
-    primaryNeedsColumns: List[Column],
-    workColumns: List[Column],
-    otherColumns: List[Column],
-    df: DataFrame
-  ): DataFrame = {
+                        primaryNeedsColumns: List[Column],
+                        workColumns: List[Column],
+                        otherColumns: List[Column],
+                        df: DataFrame
+                      ): DataFrame = {
     val workingStatusProjection: Column = expr("case when telfs >= 1 AND telfs < 3 then 'working' else 'not working' end").name("working")
     val sexProjection: Column = expr("case tesex when 1 then 'male' else 'female' end").name("sex")
     val ageProjection: Column = expr("case when 15 <= teage AND teage <= 22 then 'young' when 23 <= teage AND teage <= 55 then 'active' else 'elder' end").name("age")
 
-    val primaryNeedsProjection: Column = primaryNeedsColumns.fold(lit(0))(_+_).name("primaryNeeds")
-    val workProjection: Column = workColumns.fold(lit(0))(_+_).name("work")
-    val otherProjection: Column = otherColumns.fold(lit(0))(_+_).name("other")
+    val primaryNeedsProjection: Column = primaryNeedsColumns.fold(lit(0))(_ + _).name("primaryNeeds")
+    val workProjection: Column = workColumns.fold(lit(0))(_ + _).name("work")
+    val otherProjection: Column = otherColumns.fold(lit(0))(_ + _).name("other")
     df
-      .select(workingStatusProjection, sexProjection, ageProjection, primaryNeedsProjection.name("primaryNeeds"), workProjection, otherProjection)
+      .select(workingStatusProjection, sexProjection, ageProjection, primaryNeedsProjection, workProjection, otherProjection)
+      .where($"telfs" <= 4)
+      .cache() // Discard people who are not in labor force
+  }
+
+  def timeUsageSummary2(
+                         primaryNeedsColumns: List[Column],
+                         workColumns: List[Column],
+                         otherColumns: List[Column],
+                         df: DataFrame
+                       ): DataFrame = {
+    val workingStatusProjection: Column = expr("case when telfs >= 1 AND telfs < 3 then 'working' else 'not working' end").name("working")
+    val sexProjection: Column = expr("case tesex when 1 then 'male' else 'female' end").name("sex")
+    val ageProjection: Column = expr("case when 15 <= teage AND teage <= 22 then 'young' when 23 <= teage AND teage <= 55 then 'active' else 'elder' end").name("age")
+
+    val primaryNeedsProjection: Column = primaryNeedsColumns.fold(lit(0))(_ + _).name("primaryNeeds")
+    val workProjection: Column = workColumns.fold(lit(0))(_ + _).name("work")
+    val otherProjection: Column = otherColumns.fold(lit(0))(_ + _).name("other")
+    df
+      .select(workingStatusProjection, sexProjection, ageProjection, primaryNeedsProjection, workProjection, otherProjection)
       .where($"telfs" <= 4) // Discard people who are not in labor force
   }
 
@@ -161,21 +210,23 @@ object TimeUsage {
     *         ages of life (young, active or elder), sex and working status.
     * @param summed DataFrame returned by `timeUsageSumByClass`
     *
-    * The resulting DataFrame should have the following columns:
+    *               The resulting DataFrame should have the following columns:
     * - working: the “working” column of the `summed` DataFrame,
     * - sex: the “sex” column of the `summed` DataFrame,
     * - age: the “age” column of the `summed` DataFrame,
     * - primaryNeeds: the average value of the “primaryNeeds” columns of all the people that have the same working
-    *   status, sex and age, rounded with a scale of 1 (using the `round` function),
+    *               status, sex and age, rounded with a scale of 1 (using the `round` function),
     * - work: the average value of the “work” columns of all the people that have the same working status, sex
-    *   and age, rounded with a scale of 1 (using the `round` function),
+    *               and age, rounded with a scale of 1 (using the `round` function),
     * - other: the average value of the “other” columns all the people that have the same working status, sex and
-    *   age, rounded with a scale of 1 (using the `round` function).
+    *               age, rounded with a scale of 1 (using the `round` function).
     *
-    * Finally, the resulting DataFrame should be sorted by working status, sex and age.
+    *               Finally, the resulting DataFrame should be sorted by working status, sex and age.
     */
   def timeUsageGrouped(summed: DataFrame): DataFrame = {
-    summed.groupBy(col("working").name("status"),col("sex"),col("age")).agg(avg("primaryNeeds").name("primaryNeeds"),avg("work").name("work"),avg("other").name("other"))
+    summed
+      .groupBy(col("working"), col("sex"), col("age")).agg(round(avg("primaryNeeds"), 1).name("primaryNeeds"), round(avg("work"), 1).name("work"), round(avg("other"), 1).name("other"))
+      .orderBy(col("working"), col("sex"), col("age"))
   }
 
   /**
@@ -192,49 +243,64 @@ object TimeUsage {
     * @param viewName Name of the SQL view to use
     */
   def timeUsageGroupedSqlQuery(viewName: String): String =
-    ???
+    "select " +
+      "working, sex, age, " +
+      "round(avg(primaryNeeds),1) primaryNeeds, round(avg(work),1) work, round(avg(other),1) other " +
+      "from summed " +
+      "group by working, sex, age " +
+      "order by working, sex, age "
 
   /**
     * @return A `Dataset[TimeUsageRow]` from the “untyped” `DataFrame`
     * @param timeUsageSummaryDf `DataFrame` returned by the `timeUsageSummary` method
     *
-    * Hint: you should use the `getAs` method of `Row` to look up columns and
-    * cast them at the same time.
+    *                           Hint: you should use the `getAs` method of `Row` to look up columns and
+    *                           cast them at the same time.
     */
-  def timeUsageSummaryTyped(timeUsageSummaryDf: DataFrame): Dataset[TimeUsageRow] =
-    ???
+  def timeUsageSummaryTyped(timeUsageSummaryDf: DataFrame): Dataset[TimeUsageRow] = {
+    timeUsageSummaryDf.as[TimeUsageRow]
+  }
+
 
   /**
     * @return Same as `timeUsageGrouped`, but using the typed API when possible
     * @param summed Dataset returned by the `timeUsageSummaryTyped` method
     *
-    * Note that, though they have the same type (`Dataset[TimeUsageRow]`), the input
-    * dataset contains one element per respondent, whereas the resulting dataset
-    * contains one element per group (whose time spent on each activity kind has
-    * been aggregated).
+    *               Note that, though they have the same type (`Dataset[TimeUsageRow]`), the input
+    *               dataset contains one element per respondent, whereas the resulting dataset
+    *               contains one element per group (whose time spent on each activity kind has
+    *               been aggregated).
     *
-    * Hint: you should use the `groupByKey` and `typed.avg` methods.
+    *               Hint: you should use the `groupByKey` and `typed.avg` methods.
     */
   def timeUsageGroupedTyped(summed: Dataset[TimeUsageRow]): Dataset[TimeUsageRow] = {
     import org.apache.spark.sql.expressions.scalalang.typed
-    ???
+    def round1(d: Double) = (d * 10).round / 10d
+
+    summed
+      .groupByKey(x => (x.working, x.sex, x.age))
+      .agg(typed.avg[TimeUsageRow](_.primaryNeeds), typed.avg[TimeUsageRow](_.work), typed.avg[TimeUsageRow](_.other))
+      .map {
+        case ((working, sex, age), primaryNeeds, work, other) => TimeUsageRow(working, sex, age, round1(primaryNeeds), round1(work), round1(other))
+      }.orderBy("working", "sex", "age")
   }
 }
 
 /**
   * Models a row of the summarized data set
-  * @param working Working status (either "working" or "not working")
-  * @param sex Sex (either "male" or "female")
-  * @param age Age (either "young", "active" or "elder")
+  *
+  * @param working      Working status (either "working" or "not working")
+  * @param sex          Sex (either "male" or "female")
+  * @param age          Age (either "young", "active" or "elder")
   * @param primaryNeeds Number of daily hours spent on primary needs
-  * @param work Number of daily hours spent on work
-  * @param other Number of daily hours spent on other activities
+  * @param work         Number of daily hours spent on work
+  * @param other        Number of daily hours spent on other activities
   */
 case class TimeUsageRow(
-  working: String,
-  sex: String,
-  age: String,
-  primaryNeeds: Double,
-  work: Double,
-  other: Double
-)
+                         working: String,
+                         sex: String,
+                         age: String,
+                         primaryNeeds: Double,
+                         work: Double,
+                         other: Double
+                       )
--- timeusage/src/test/scala/timeusage/TimeUsageSuite.scala
+++ timeusage/src/test/scala/timeusage/TimeUsageSuite.scala
@@ -1,19 +1,131 @@
 package timeusage
 
-import org.apache.spark.sql.{ColumnName, DataFrame, Row}
-import org.apache.spark.sql.types.{
-  DoubleType,
-  StringType,
-  StructField,
-  StructType
-}
 import org.junit.runner.RunWith
 import org.scalatest.junit.JUnitRunner
 import org.scalatest.{BeforeAndAfterAll, FunSuite}
-
-import scala.util.Random
+import timeusage.TimeUsage._
 
 @RunWith(classOf[JUnitRunner])
 class TimeUsageSuite extends FunSuite with BeforeAndAfterAll {
+  //    test("take .01") {
+  //      val (columns, initDf) = read("/timeusage/atussum.csv", .01)
+  //      val (primaryNeedsColumns, workColumns, otherColumns) = classifiedColumns(columns)
+  //      val summaryDf = timeUsageSummary(primaryNeedsColumns, workColumns, otherColumns, initDf)
+  //      summaryDf.show()
+  //      val finalDf = timeUsageGrouped(summaryDf)
+  //      finalDf.show()
+  //    }
 
+
+  //  test("t1") {
+  //    val df = rdd("/timeusage/atussum.csv")
+  //    df.take(5).map(println(_))
+  //
+  //    val (columns, initDf) = read("/timeusage/atussum.csv", .01)
+  //    initDf.show(5)
+  //    val (primaryNeedsColumns, workColumns, otherColumns) = classifiedColumns(columns)
+  //
+  //    val summaryDf = timeUsageSummary2(primaryNeedsColumns, workColumns, otherColumns, initDf)
+  //    summaryDf.show()
+  //    val finalDf = timeUsageGrouped(summaryDf)
+  //    finalDf.show()
+  //  }
+
+  //  test("take .01 t1") {
+  //    val df = rdd("/timeusage/atussum.csv")
+  //    df.take(5).map(println(_))
+  //    val (columns, initDf) = read("/timeusage/atussum.csv", .01)
+  //    initDf.show(10)
+  //    //    val finalDf = timeUsageGrouped(summaryDf)
+  //    //    finalDf.show()
+  //  }
+  //
+  //
+  //  test("take .01 t2") {
+  //    val df = rdd("/timeusage/atussum.csv")
+  //    df.take(5).map(println(_))
+  //    val (columns, initDf) = read("/timeusage/atussum.csv", .01)
+  //
+  //
+  //    val (primaryNeedsColumns, workColumns, otherColumns) = classifiedColumns(columns)
+  //    val summaryDf = timeUsageSummary(primaryNeedsColumns, workColumns, otherColumns, initDf)
+  //    summaryDf.show()
+  //  }
+
+  test("t2") {
+    val df = rdd("/timeusage/atussum.csv")
+    //    df.take(5).map(println(_))
+
+    val (columns, initDf) = read("/timeusage/atussum.csv", .01)
+    //    initDf.show(5)
+    val (primaryNeedsColumns, workColumns, otherColumns) = classifiedColumns(columns)
+
+    val summaryDf = timeUsageSummary(primaryNeedsColumns, workColumns, otherColumns, initDf).cache()
+
+    // timeUsageGrouped && timeUsageGroupedSql && timeUsageGroupedTyped
+    val finalDf = timeUsageGrouped(summaryDf)
+    val x1 = finalDf.take(20)
+    val finalDf2 = timeUsageGroupedSql(summaryDf)
+    val x2 = finalDf2.take(20)
+    val finalDf3 = timeUsageGroupedTyped(timeUsageSummaryTyped(summaryDf))
+    val x3 = finalDf3.take(20)
+    println("timeUsageGrouped")
+    x1.foreach(println(_))
+    println("timeUsageGroupedSql")
+    x2.foreach(println(_))
+    println("timeUsageGroupedTyped")
+    x3.foreach(println(_))
+    assert(x1.sameElements(x2))
+    assert(x1.map(x=>(x.get(0),x.get(1),x.get(2),x.get(3),x.get(4),x.get(5))).sameElements(x3.map(x=>(x.working,x.sex,x.age,x.primaryNeeds,x.work,x.other))))
+  }
+  //  +-----------+------+------+------------+-----+-----+
+  //  |    working|   sex|   age|primaryNeeds| work|other|
+  //  +-----------+------+------+------------+-----+-----+
+  //  |not working|female| young|       723.9| 74.6|625.3|
+  //    |    working|female|active|       703.5|233.7|490.3|
+  //  |not working|female| elder|       585.6| 18.3|795.6|
+  //    |    working|  male| elder|       618.3|269.3|544.2|
+  //  |    working|female| elder|       630.4|247.5|548.9|
+  //  |not working|  male|active|       658.8| 19.7|741.2|
+  //    |not working|  male| young|       621.7|  0.0|816.1|
+  //    |    working|  male|active|       647.0|300.8|482.5|
+  //  |not working|female|active|       764.4| 11.7|657.4|
+  //    |    working|female| young|       728.2|198.5|481.0|
+  //  |not working|  male| elder|       576.3|170.3|693.2|
+  //    |    working|  male| young|       650.3|171.1|604.1|
+  //  +-----------+------+------+------------+-----+-----+
+  //  timeUsageGroupedSql
+  // +-----------+------+------+------------+-----+-----+
+  //  |    working|   sex|   age|primaryNeeds| work|other|
+  //  +-----------+------+------+------------+-----+-----+
+  //  |not working|female| young|       723.9| 74.6|625.3|
+  //    |    working|female|active|       703.5|233.7|490.3|
+  //  |not working|female| elder|       585.6| 18.3|795.6|
+  //    |    working|  male| elder|       618.3|269.3|544.2|
+  //  |    working|female| elder|       630.4|247.5|548.9|
+  //  |not working|  male|active|       658.8| 19.7|741.2|
+  //    |not working|  male| young|       621.7|  0.0|816.1|
+  //    |    working|  male|active|       647.0|300.8|482.5|
+  //  |not working|female|active|       764.4| 11.7|657.4|
+  //    |    working|female| young|       728.2|198.5|481.0|
+  //  |not working|  male| elder|       576.3|170.3|693.2|
+  //    |    working|  male| young|       650.3|171.1|604.1|
+  //  +-----------+------+------+------------+-----+-----+
+  //  timeUsageGroupedTyped
+  // +-----------+------+------+------------+-----+-----+
+  //  |    working|   sex|   age|primaryNeeds| work|other|
+  //  +-----------+------+------+------------+-----+-----+
+  //  |not working|female|active|       764.4| 11.7|657.4|
+  //    |not working|female| elder|       585.6| 18.3|795.6|
+  //    |not working|female| young|       723.9| 74.6|625.3|
+  //    |not working|  male|active|       658.8| 19.7|741.2|
+  //    |not working|  male| elder|       576.3|170.3|693.2|
+  //    |not working|  male| young|       621.7|  0.0|816.1|
+  //    |    working|female|active|       703.5|233.7|490.3|
+  //  |    working|female| elder|       630.4|247.5|548.9|
+  //  |    working|female| young|       728.2|198.5|481.0|
+  //  |    working|  male|active|       647.0|300.8|482.5|
+  //  |    working|  male| elder|       618.3|269.3|544.2|
+  //  |    working|  male| young|       650.3|171.1|604.1|
+  //  +-----------+------+------+------------+-----+-----+
 }
